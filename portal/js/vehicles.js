df5a69b6f63de7546f999a6a558f68f0f7c5c3ff
Pasalo-Cars-PH <derlonskie0929@gmail.com>
2026-07-14 19:02:44 +0800

    Create vehicles.js

---
+++ portal/js/vehicles.js
@@ -0,0 +1,43 @@
+let vehicles = [];
+
+fetch("../data/vehicles.json")
+.then(res => res.json())
+.then(data => {
+    vehicles = data;
+    displayVehicles(data);
+});
+
+function displayVehicles(data){
+
+    const container =
+    document.getElementById("vehicle-list");
+
+    container.innerHTML = "";
+
+    data.forEach(vehicle => {
+
+        container.innerHTML += `
+        <div class="card">
+            <h3>${vehicle.brand} ${vehicle.model}</h3>
+            <p>Year: ${vehicle.year}</p>
+            <p>Monthly: ₱${vehicle.monthly.toLocaleString()}</p>
+            <p>Status: ${vehicle.status}</p>
+        </div>
+        `;
+    });
+}
+
+function filterVehicles(){
+
+    const keyword =
+    document.getElementById("search")
+    .value
+    .toLowerCase();
+
+    const filtered =
+    vehicles.filter(v =>
+      v.brand.toLowerCase().includes(keyword)
+    );
+
+    displayVehicles(filtered);
+}
